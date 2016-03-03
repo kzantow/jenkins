152da4e116f52a74098558066e95256370e3f742
kzantow <kzantow@gmail.com>
2016-03-03 03:11:00 -0500

    Revert whitespace change

--- core/src/main/java/hudson/PluginManager.java
+++ core/src/main/java/hudson/PluginManager.java
@@ -839,31 +839,31 @@ public abstract class PluginManager extends AbstractModelObject implements OnMas
      * Creates a hudson.PluginStrategy, looking at the corresponding system property.
      */
     protected PluginStrategy createPluginStrategy() {
-        String strategyName = System.getProperty(PluginStrategy.class.getName());
-        if (strategyName != null) {
-            try {
-                Class<?> klazz = getClass().getClassLoader().loadClass(strategyName);
-                Object strategy = klazz.getConstructor(PluginManager.class)
-                        .newInstance(this);
-                if (strategy instanceof PluginStrategy) {
-                    LOGGER.info("Plugin strategy: " + strategyName);
-                    return (PluginStrategy) strategy;
-                } else {
-                    LOGGER.warning("Plugin strategy (" + strategyName +
-                            ") is not an instance of hudson.PluginStrategy");
-                }
-            } catch (ClassNotFoundException e) {
-                LOGGER.warning("Plugin strategy class not found: "
-                        + strategyName);
-            } catch (Exception e) {
-                LOGGER.log(WARNING, "Could not instantiate plugin strategy: "
-                        + strategyName + ". Falling back to ClassicPluginStrategy", e);
-            }
-            LOGGER.info("Falling back to ClassicPluginStrategy");
-        }
+		String strategyName = System.getProperty(PluginStrategy.class.getName());
+		if (strategyName != null) {
+			try {
+				Class<?> klazz = getClass().getClassLoader().loadClass(strategyName);
+				Object strategy = klazz.getConstructor(PluginManager.class)
+						.newInstance(this);
+				if (strategy instanceof PluginStrategy) {
+					LOGGER.info("Plugin strategy: " + strategyName);
+					return (PluginStrategy) strategy;
+				} else {
+					LOGGER.warning("Plugin strategy (" + strategyName +
+							") is not an instance of hudson.PluginStrategy");
+				}
+			} catch (ClassNotFoundException e) {
+				LOGGER.warning("Plugin strategy class not found: "
+						+ strategyName);
+			} catch (Exception e) {
+				LOGGER.log(WARNING, "Could not instantiate plugin strategy: "
+						+ strategyName + ". Falling back to ClassicPluginStrategy", e);
+			}
+			LOGGER.info("Falling back to ClassicPluginStrategy");
+		}
 
-        // default and fallback
-        return new ClassicPluginStrategy(this);
+		// default and fallback
+		return new ClassicPluginStrategy(this);
     }
 
     public PluginStrategy getPluginStrategy() {
@@ -1021,8 +1021,8 @@ public abstract class PluginManager extends AbstractModelObject implements OnMas
             for (UpdateSite.Plugin plugin: site.getAvailables()) {
                 JSONObject pluginInfo = allPlugins.get(plugin.name);
                 if(pluginInfo == null) {
-            pluginInfo = new JSONObject();
-            pluginInfo.put("installed", false);
+			pluginInfo = new JSONObject();
+			pluginInfo.put("installed", false);
                 }
                 pluginInfo.put("name", plugin.name);
                 pluginInfo.put("title", plugin.getDisplayName());
@@ -1176,7 +1176,7 @@ public abstract class PluginManager extends AbstractModelObject implements OnMas
                 public void run() {
                     INSTALLING: while (true) {
                         try {
-                updateCenter.persistInstallStatus();
+				updateCenter.persistInstallStatus();
                             Thread.sleep(500);
                             for (Future<UpdateCenter.UpdateCenterJob> jobFuture : installJobs) {
                                 if(!jobFuture.isDone() && !jobFuture.isCancelled()) {
@@ -1188,7 +1188,7 @@ public abstract class PluginManager extends AbstractModelObject implements OnMas
                         }
                         break;
                     }
-            updateCenter.persistInstallStatus();
+			updateCenter.persistInstallStatus();
                     jenkins.setInstallState(InstallState.INITIAL_PLUGINS_INSTALLED);
                     InstallUtil.saveLastExecVersion();
                 }
